fe3ec7d533a7af3c12d84fad55a24477cecda29d
John Rae <john.rae23@gmail.com>
2015-10-12 13:20:20 -0400

    Moved over HTML, SCSS, and JS from assignment 12

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 (function () {
 
-  console.log('It Works!');
+  var string = $('#contentSpit').text();
+
+  var funStuff = _.template(string);
+
+
+  _.each(etsy.results, function(item){
+    var itemHTML = funStuff(item);
+  $('.content').append(itemHTML);
+  });
+
 
 }());
