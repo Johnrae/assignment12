8ee420367f54e558e5e66fe70386796960078f6d
John Rae <john.rae23@gmail.com>
2015-10-12 15:12:40 -0400

    added js to link to etsy server

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 (function () {
 
-  var string = $('#contentSpit').text();
 
-  var funStuff = _.template(string);
+  let etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=slime&includes=Images,Shop';
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then (function (etsy) {
+
+      var string = $('#contentSpit').text();
+
+      var funStuff = _.template(string);
 
 
-  _.each(etsy.results, function(item){
-    var itemHTML = funStuff(item);
-  $('.content').append(itemHTML);
+      _.each(etsy.results, function(item){
+        var itemHTML = funStuff(item);
+      $('.content').append(itemHTML);
+      });
+      
+    console.log(etsy);
   });
 
 
